79d602333a8090fdc55ff6026346bb1419b8d5de
edo37 <edoardo0307@gmail.com>
2025-05-21 20:23:12 +0200

    added file with the main page titles for sidebar

---
+++ src/constants/SidebarContent/homeSidebarTitles.jsx
@@ -0,0 +1,10 @@
+const homeSidebarTitles = [
+  { id: "0", title: " Cos'è una console?" },
+  { id: "1", title: "Gli Inizi" },
+  { id: "2", title: "La crisi del 1983" },
+  { id: "3", title: "Gli eventi più importanti" },
+  { id: "4", title: "Le generazioni" },
+  { id: "5", title: "Tutte le Console di Sony, Microsoft e Nintendo" },
+];
+
+export default homeSidebarTitles;
