6a9d742fec31cfe4bb4c5acd7be39b0787af7a2a
edo37 <edoardo0307@gmail.com>
2025-05-21 20:23:27 +0200

    added file with the Microsoft page titles for sidebar

---
+++ src/constants/SidebarContent/microsoftSidebarTitles.jsx
@@ -0,0 +1,13 @@
+const microsoftSidebarTitles = [
+  { id: "0", title: "Xbox" },
+  { id: "1", title: "Xbox 360" },
+  { id: "2", title: "Xbox 360 S" },
+  { id: "3", title: "Xbox 360 E" },
+  { id: "4", title: "Xbox One" },
+  { id: "5", title: "Xbox One S" },
+  { id: "6", title: "Xbox One X" },
+  { id: "7", title: "Xbox Series X" },
+  { id: "8", title: "Xbox Series S" },
+];
+
+export default microsoftSidebarTitles
