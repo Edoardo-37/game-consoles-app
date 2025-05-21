2c4791c5f6d99d61b29f418dcae4c2d68a0186bd
edo37 <edoardo0307@gmail.com>
2025-05-21 20:24:11 +0200

    added file with the Sony page titles for sidebar

---
+++ src/constants/SidebarContent/sonySidebarTitles.jsx
@@ -0,0 +1,20 @@
+const sonySidebarTitles = [
+  { id: "0", title: "PlayStation" },
+  { id: "1", title: "PocketStation" },
+  { id: "2", title: "PlayStation 2" },
+  { id: "3", title: "PS One" },
+  { id: "4", title: "PlayStation 2 Slim" },
+  { id: "5", title: "PlayStation Portable" },
+  { id: "6", title: "PlayStation 3" },
+  { id: "7", title: "PlayStation 3 Slim" },
+  { id: "8", title: "PlayStation Vita" },
+  { id: "9", title: "PlayStation 3 Super Slim" },
+  { id: "10", title: "PlayStation 4" },
+  { id: "11", title: "PlayStation 4 Slim" },
+  { id: "12", title: "PlayStation 4 Pro" },
+  { id: "13", title: "PlayStation 5" },
+  { id: "14", title: "PlayStation 5 Slim" },
+  { id: "15", title: "PlayStation 5 Pro" },
+];
+
+export default sonySidebarTitles;
