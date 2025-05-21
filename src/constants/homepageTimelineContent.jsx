d2b006daaf8412370b2caba61ba8d6d4d25a8c0a
edo37 <edoardo0307@gmail.com>
2025-05-21 20:25:13 +0200

    added file with content for the main page timeline

---
+++ src/constants/homepageTimelineContent.jsx
@@ -0,0 +1,50 @@
+export const homepageTimelineContent = [
+  {
+    id: 0,
+    year: 1972,
+    title: "Magnavox Odyssey",
+    text: "Prima console videoludica da casa molto semplice: non aveva audio e i giochi erano basati sulla sovrapposizione di pellicole trasparenti sulla TV.",
+  },
+  {
+    id: 1,
+    year: 1983,
+    title: "Nintendo Famicom / NES (Nintendo Entertainment System)",
+    text: "Nintendo lancia il Famicom in Giappone (NES in USA nel 1985), rivoluzionando il mercato dopo il crollo dell'industria nel 1983. Nascono franchise come Super Mario Bros, The Legend of Zelda e Metroid.",
+  },
+  {
+    id: 2,
+    year: 1989,
+    title: "Sega Genesis / Mega Drive",
+    text: "Sega entra in competizione con giochi più 'maturi' e un approccio più aggressivo. Comincia la storica rivalità con Nintendo: la competizione la si notava negli spot pubblicitari in cui ognuna cercava di far capire che il quanto il proprio prodotto fosse il più potente.",
+  },
+  {
+    id: 3,
+    year: 1994,
+    title: "Sony PlayStation",
+    text: "Sony debutta con una console basata su CD-ROM che permise l'ascesa della grafica 3D con grandi titoli come Final Fantasy VII e Metal Gear Solid. Il suo debutto fu una rivoluzione che cambiò le regole del settore.",
+  },
+  {
+    id: 4,
+    year: 2001,
+    title: "Microsoft Xbox",
+    text: "Microsoft entra nel mercato console con una macchina potente e lancia Halo, che diventa un franchise subito apprezzato. Da qui inizia l'era moderna delle console.",
+  },
+  {
+    id: 5,
+    year: 2006,
+    title: "Nintendo Wii",
+    text: "La console che introdusse i controlli di movimento: ebbe un enorme successo grazie alla sua semplicità d'uso adatta a tutta la famiglia e la volontà di Nintendo di non voler più correre dietro le competitor, ma diventare un mercato a parte. L'introduzione del telecomando Wii come controller portò Microsoft e Sony a cercare di replicarne il successo: la prima con il Kinect e Sony con i controller Playstation Move.",
+  },
+  {
+    id: 6,
+    year: 2013,
+    title: "PS4 e Xbox One",
+    text: "Si consolida in questa ottava generazione il gioco online e gli acquisti in digitale. PS4 domina la generazione con titoli esclusivi come The Last of Us Part II e God of War (2018), Marvel Spiderman. Ciò che spinse i consumatori a preferire la console Sony fu il prezzo: Microsoft con Xbox One fece il grande errore di includere obbligatoriamente il Kinect con la console, facendo lievitare il prezzo e risultando più costosa della concorrente. Inoltre il sistema operativo della PS4 era più semplice da utilizzare a differenza della Xbox che riprendeva la filosofia di Windows 8 e la sua Metro UI." ,
+  },
+  {
+    id: 7,
+    year: 2020,
+    title: "PS5 e Xbox Series X/S",
+    text: "La nona generazione offre SSD veloci che assicurano tempi di caricamento azzerati, ray tracing e maggior continuità con il digitale e i servizi online (Xbox Game Pass, PS Plus).",
+  }
+];
