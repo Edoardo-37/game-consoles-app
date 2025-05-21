e643bac2e8ee1e2c606cc6d52617dad69bbc876a
edo37 <edoardo0307@gmail.com>
2025-05-21 20:19:46 +0200

    added PageLinks component

---
+++ src/components/PageLinks.jsx
@@ -0,0 +1,11 @@
+const PageLinks = () => {
+  return (
+    <>
+      <a title="Vai alla pagina dedicata alla storia delle console Nintendo" className='text-error font-bold hover:underline my-4 p-4' href="/nintendo-consoles">Nintendo</a>
+      <a title="Vai alla pagina dedicata alla storia delle console Sony" className='text-info font-bold hover:underline my-4 p-4' href="/sony-consoles">Sony</a>
+      <a title="Vai alla pagina dedicata alla storia delle console Microsoft" className='text-success font-bold hover:underline my-4 p-4' href="/microsoft-consoles">Microsoft</a>
+    </>
+
+  )
+}
+export default PageLinks
