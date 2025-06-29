6511fa4170b6e7107186e3c64488c19040f820ea
edo37 <edoardo0307@gmail.com>
2025-06-29 18:14:49 +0200

    changed Links tag for optimization

--- src/components/PageLinks.jsx
+++ src/components/PageLinks.jsx
@@ -1,11 +1,30 @@
+import { Link } from "react-router-dom";
+
 const PageLinks = () => {
   return (
     <>
-      <a title="Vai alla pagina dedicata alla storia delle console Nintendo" className='text-error font-bold hover:underline my-4 p-4' href="/nintendo-consoles">Nintendo</a>
-      <a title="Vai alla pagina dedicata alla storia delle console Sony" className='text-info font-bold hover:underline my-4 p-4' href="/sony-consoles">Sony</a>
-      <a title="Vai alla pagina dedicata alla storia delle console Microsoft" className='text-success font-bold hover:underline my-4 p-4' href="/microsoft-consoles">Microsoft</a>
+      <Link
+        title="Vai alla pagina dedicata alla storia delle console Nintendo"
+        className="text-error font-bold hover:underline my-4 p-4"
+        to="/nintendo-consoles"
+      >
+        Nintendo
+      </Link>
+      <Link
+        title="Vai alla pagina dedicata alla storia delle console Sony"
+        className="text-info font-bold hover:underline my-4 p-4"
+        to="/sony-consoles"
+      >
+        Sony
+      </Link>
+      <Link
+        title="Vai alla pagina dedicata alla storia delle console Microsoft"
+        className="text-success font-bold hover:underline my-4 p-4"
+        to="/microsoft-consoles"
+      >
+        Microsoft
+      </Link>
     </>
-
-  )
-}
-export default PageLinks
+  );
+};
+export default PageLinks;
