15a9ce50292d30063b016693f5e43fe9f6952258
edo37 <edoardo0307@gmail.com>
2025-06-29 18:37:02 +0200

    added page scroller component in BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,16 @@ import App from "./App.jsx";
 import Nintendo from "./pages/Nintendo.jsx";
 import Sony from "./pages/Sony.jsx";
 import Microsoft from "./pages/Microsoft.jsx";
+import ScrollToTop from "./components/ScrollToTop.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<App />} />
-      <Route path="/nintendo-consoles" element={<Nintendo/>} />
-      <Route path="/sony-consoles" element={<Sony/>} />
-      <Route path="/microsoft-consoles" element={<Microsoft/>} />
-  </Routes>
+    <ScrollToTop /> 
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/nintendo-consoles" element={<Nintendo />} />
+      <Route path="/sony-consoles" element={<Sony />} />
+      <Route path="/microsoft-consoles" element={<Microsoft />} />
+    </Routes>
   </BrowserRouter>
 );
